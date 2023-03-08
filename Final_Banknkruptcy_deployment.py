d4f6eb5c4627b74dce0855d9b0cb5ff6c222c2ff
MD AMAN ALAM <98773885+mdamanalam@users.noreply.github.com>
2023-03-08 20:26:50 +0530

    Add files via upload

---
+++ Final_Banknkruptcy_deployment.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon jan 02 18:40:08 2023
+@author: Atharv Chougule.
+"""
+
+# -*- coding: utf-8 -*-
+"""
+"""
+
+import numpy as np
+import pickle
+import streamlit as st
+
+st.title('Bankruptcy Prediction Web App')
+st.sidebar.header('User Input Parameters')
+
+# loding the saved model
+loaded_model = pickle.load(open('C:/Users/91808/OneDrive/Desktop/trained_model.sav','rb'))
+
+input_data = (0,0,0.5,0.5,0.5,0)
+
+
+# creating a function for prediction
+
+def bankruptcy_prediction(input_data):
+    
+    
+
+    #changing the input data to numpy array
+    input_data_as_numpy_array = np.asarray(input_data)
+
+    #reshape the array as we are predicting for  one instance
+    input_data_reshape = input_data_as_numpy_array.reshape(1,-1)
+
+    prediction = loaded_model.predict(input_data_reshape)
+    print(prediction)
+
+    if (prediction[0] == 0):
+      return'The company is going to bankrupt'
+    else:
+      return'The company is non-bankrupt'
+      
+      
+def main():
+
+   # giving a title 
+   st.title('User Input Parameters')
+   
+   #getting the input data from the user
+   
+      
+   industrial_risk = st.text_input('Parameter for industrial_risk')
+   management_risk = st.text_input('Parameter for management_risk')
+   financial_flexibility = st.text_input('Parameter for financial_flexibility')
+   credibility = st.text_input('Parameter for credibility')
+   competitiveness = st.text_input('Parameter for competitiveness')
+   operating_risk = st.text_input('Parameter for operating_risk')
+   
+   # code for prediction
+   bankruptcy = ''
+   
+   # creating a button for prediction
+   
+   if st.button('Bankruptcy Result'):
+       bankruptcy = bankruptcy_prediction([industrial_risk,management_risk,financial_flexibility,credibility,competitiveness,operating_risk])
+       
+   st.success(bankruptcy)
+
+
+if __name__ == '__main__':
+    main()
